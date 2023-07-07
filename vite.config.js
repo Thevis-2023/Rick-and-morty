7ebff9260f95b7e6d9e69ffcf05a8692814a15ef
Andres <andresgueno@hotmail>
2023-07-07 11:28:13 -0500

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/Thevis-2023/Rick-and-morty",
+  base: "/Rick-and-morty/",
   plugins: [react()],
 })
